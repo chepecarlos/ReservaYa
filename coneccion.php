5995ca98bdc5528b2b4cfde6919c376b51457b96
chepecarlos <chepecarlos@alswblog.org>
2012-03-04 17:30:06 -0600

    Lista el archivo de conexion

--- coneccion.php
+++ coneccion.php
@@ -2,4 +2,5 @@
 //Conexion con mysql
 require("configuracion.php");
 $conexion=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Problemas con la conexcion");
+mysql_select_db(DB_NAME,$conexion) or die("Problemas en la selección de la base de datos");
 ?>
