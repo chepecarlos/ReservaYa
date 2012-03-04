5287ffe1e0064a79e7dd0e603a17ebb834cb908a
Evorock <evorock@alswblog.org>
2012-03-04 15:18:15 -0600

    Formulario de Empresa

--- instalacion/paso1.php
+++ instalacion/paso1.php
@@ -9,6 +9,9 @@
   <p>Email :
     <input type="text" name="Titulo3" />
 </p>
+  <p>Website :
+    <input type="text" name="Titulo6" />
+  </p>
   <p>Telefono:
     <input type="text" name="Titulo4" />
   </p>
@@ -16,10 +19,10 @@
     <input type="text" name="Titulo5" />
   </p>
   <p>    <br>
+  	<input type="reset" name="button" id="button" value="Limpiar" />
     <?php
       echo '<a href="'.$_SERVER['HTTP_REFERER'].'"><input type="button" name="button" id="button" value="Atras" /></a>';
 	?>
-    <input type="reset" name="button" id="button" value="Restablecer" />
     <input type="submit" value="Siquiente" />
   </p>
 </form>
