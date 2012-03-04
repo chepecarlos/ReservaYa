6a6a0dffa7053e877d32e6ec034e100df19f8f82
Evorock <evorock@alswblog.org>
2012-03-04 14:11:14 -0600

    Formulario 2

--- instalacion/paso1.php
+++ instalacion/paso1.php
@@ -1,8 +1,25 @@
-Paso 1
-
+<p>Registro de Datos de la Empresa</p>
 <form method="post" action="?Paso=777">
-Titulo de la pagina: 
-<input type="text" name="Titulo">
-<br>
-<input type="submit" value="Siquiente">
+  <p>Nombre:
+    <input type="text" name="Titulo">
+  </p>
+  <p>Direccion :
+    <input type="text" name="Titulo2" />
+</p>
+  <p>Email :
+    <input type="text" name="Titulo3" />
+</p>
+  <p>Telefono:
+    <input type="text" name="Titulo4" />
+  </p>
+  <p>Slogan:
+    <input type="text" name="Titulo5" />
+  </p>
+  <p>    <br>
+    <?php
+      echo '<a href="'.$_SERVER['HTTP_REFERER'].'"><input type="button" name="button" id="button" value="Atras" /></a>';
+	?>
+    <input type="reset" name="button" id="button" value="Restablecer" />
+    <input type="submit" value="Siquiente" />
+  </p>
 </form>
