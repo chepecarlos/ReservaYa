8490ad6fd97e1a3c144691f6165d12960b716bcb
Evorock <evorock@alswblog.org>
2012-03-04 17:01:17 -0600

    modificacion de foteer

--- pie.php
+++ pie.php
@@ -1,5 +1,9 @@
-<?php
-/* el final de la pagina*/
-echo "soy el pie de pagina"
-?>
+<table width="80%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>Home</td>
+    <td>Login</td>
+    <td>Contactos</td>
+    <td>FAQ</td>
+  </tr>
+</table>
 
