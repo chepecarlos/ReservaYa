8c63d7a2ad0fa12957787faee65c528936f9517f
Evorock <evorock@alswblog.org>
2012-03-04 16:18:51 -0600

    Creacion de carpeta admin

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<?php  
+/* pagina de inicio del sistema */
+require("coneccion.php"); 
+?>
+<html>
+<head>
+<title>
+Proyecto ADSII
+</title>
+</head>
+<body> 
+<div>
+<?php 
+require("cabecera.php");
+?>
+<div>
+Hola mundo
+</div>
+<?php
+require("pie.php");
+?>
+</div>
+</body> 
+</html>
