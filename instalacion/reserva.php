361d7daa27405c3df6e8f5406f466b1732eedf3b
Evorock <evorock@alswblog.org>
2012-03-04 16:03:09 -0600

    Creacion de una disque pagina de reserva

---
+++ instalacion/reserva.php
@@ -0,0 +1,22 @@
+<p>Reservas Online</p>
+<form name="form1" method="post" action="">
+  <p>Nombre : 
+    <input type="text" name="textfield" id="textfield">
+  </p>
+  <p>Apellido : 
+    <input type="text" name="textfield2" id="textfield2">
+  </p>
+  <p>Email (opcional) :
+    <input type="text" name="4" id="4">
+  </p>
+  <p>1+1 =  
+    <input type="text" name="textfield3" id="textfield3">
+  </p>
+  <p>Fecha de Reserva : 
+    <input type="text" name="textfield4" id="textfield4">
+  </p>
+  <p>
+    <input type="submit" name="button" id="button" value="Enviar">
+  </p>
+</form>
+<p>&nbsp;</p>
