e88ac9a505fa24c4758d3478aebfdf43675aa215
Evorock <evorock@alswblog.org>
2012-03-04 13:26:08 -0600

    Agrege Tabla de configuracion y Ordenamiento de Comentarios

--- instalacion/Base.sql
+++ instalacion/Base.sql
@@ -1,25 +1,8 @@
 -- Base de datos para el proyecto de ADS II
+
 -- Tabla Cliente
 -- * Esta tabla nos ayudara para manejar la reserva
 -- * de mesas de dicho restaurante
--- Tabla Empleado
--- * Aqui se guardaran la informacion de cada empleado
--- * y administrador que trabaje en el restaurante
--- Tabla Mesa
--- * Aqui asignaremos la cantidad de mesas que tendra
--- * el restaurante por personas, ayudara para la 
--- * disponibilidad de mesas que hay en x momento
--- Tabla casual
--- * Si existe usuario que no hagan reservas y llegue
--- * en x momento al restaurante, se ocupara mas que 
--- * todo para mineria de datos
--- Tabla Reserva
--- * Aqui es para ver la infomracion de la reserva
--- * fecha que se realizo, dia, etc.
--- Tabla recepcion
--- * Verificara que los datos de la reserva sean correctos
--- * y agregar datos para hacer una mineria de datos.
-
 drop table if exists cliente;
 create table cliente(
   id_cliente integer auto_increment not null,
@@ -29,7 +12,9 @@ create table cliente(
   primary key (id_cliente)
  );
 
-
+-- Tabla Empleado
+-- * Aqui se guardaran la informacion de cada empleado
+-- * y administrador que trabaje en el restaurante
 drop table if exists empleado;
 create table empleado(
   id_empleado integer auto_increment not null,
@@ -44,7 +29,10 @@ create table empleado(
   primary key (id_empleado)
 );
 
-
+-- Tabla Mesa
+-- * Aqui asignaremos la cantidad de mesas que tendra
+-- * el restaurante por personas, ayudara para la 
+-- * disponibilidad de mesas que hay en x momento
 drop table if exists mesa;
 create table mesa(
   num_personas_x_mesa int(11) not null,
@@ -52,6 +40,10 @@ create table mesa(
   primary key (num_personas_x_mesa)
 );
 
+-- Tabla casual
+-- * Si existe usuario que no hagan reservas y llegue
+-- * en x momento al restaurante, se ocupara mas que 
+-- * todo para mineria de datos
 drop table if exists casual;
 create table casual(
   id_casual integer auto_increment not null,
@@ -61,7 +53,9 @@ create table casual(
   foreign key (num_personas_x_mesa) references mesa (num_personas_x_mesa)
 );
 
-
+-- Tabla Reserva
+-- * Aqui es para ver la infomracion de la reserva
+-- * fecha que se realizo, dia, etc.
 drop table if exists reserva;
 create table reserva(
   id_reserva integer auto_increment not null,
@@ -75,7 +69,9 @@ create table reserva(
   foreign key (id_cliente) references cliente (id_cliente)
  );
 
-
+-- Tabla recepcion
+-- * Verificara que los datos de la reserva sean correctos
+-- * y agregar datos para hacer una mineria de datos.
 drop table if exists recepcion;
 create table recepcion(
   id_cliente integer not null,
@@ -86,4 +82,13 @@ create table recepcion(
   foreign key (id_empleado) references empleado (id_empleado),
   foreign key (id_reserva) references reserva (id_reserva),
   foreign key (id_casual) references casual (id_casual)
+);
+
+-- Tabla config
+-- * Aqui estara las configuraciones del sistema,
+-- * Datos de la empresa, direccion, etc.
+drop table if exists config;
+create table config(
+  nombre varchar(100) not null primary key,
+  dato   varchar(200) not null
 );
