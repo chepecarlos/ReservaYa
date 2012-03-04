af8fadf4493bad1b61699bf0e1b99a8419620bfe
chepecarlos <chepecarlos@alswblog.org>
2012-03-04 13:49:17 -0600

    Mejorar el ejecutar de .sql

--- funciones.php
+++ funciones.php
@@ -11,23 +11,30 @@ function ejecutarSQL($_ArchivoSQL, $_conexionDB)
   $Separador = ";";
   
   for($i = 0; $i < $CantidadPedasos; $i++){
-	echo $Pedasos[$i];  
 	echo '<br>';
+	echo $Pedasos[$i];
+	echo ' ';  
+	echo $ConsultaSQL;
       $MiniSQL = strtolower($Pedasos[$i]);
 	  $TipoSentencia = in_array($MiniSQL, array( 
 	 'select', 'update', 'delete', 'insert',
-     'delimiter', 'create', 'alter', 'drop', 
-     'call', 'set', 'use' ));
-	}
-    if($HacerSQL) { // Si estoy parseando una sentencia me fijo si lo que viene es un delimitador para terminar la consulta
+     'create', 'alter', 'drop', 'call', 'set', 'use' ));
+    if($HacerSQL){ 
 		if($Pedasos[$i] == $Separador || substr(trim($Pedasos[$i]), -1*(strlen($Separador))) == $Separador) { 
-		// Si terminamos el parseo ejecuto la consulta
-		$ConsultaSQL .= str_replace($Separador, '', $Pedasos[$i]); // Elimino el delimitador
-		/*$_db->query($query);// la consulta*/ 
-		$ConsultaSQL = ""; // Preparo la consulta para continuar con la siguiente sentencia
-		$Pedasos[$i] = '';
-		$HacerSQL = false;
-    }
- }
+			// Si terminamos el parseo ejecuto la consulta
+			$ConsultaSQL .= str_replace($Separador, '', $Pedasos[$i]); // Elimino el delimitador
+			//$_conexionDB->query($ConsultaSQL);// la consulta 
+			echo $ConsultaSQL
+			$ConsultaSQL = ""; // Preparo la consulta para continuar con la siguiente sentencia
+			$Pedasos[$i] = '';
+			$HacerSQL = false;
+			}
+		}
+	else if($TipoSentencia){
+			$HacerSQL = true;
+			$ConsultaSQL = "";
+		}
+	$ConsultaSQL = 	"{$Pedasos[$i]} ";
+	}//fin del for	
 }/* Resive un archivo SQL, una conexion y ejecuta la sentencia */
 ?>
