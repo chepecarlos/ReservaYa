ab1c66745fe0f5d3056cf62a86c0898244d895d8
Evorock <evorock@alswblog.org>
2012-03-04 13:03:08 -0600

    Agrege Texto

--- instalacion/paso0.php
+++ instalacion/paso0.php
@@ -1,9 +1,14 @@
+<style type="text/css">
+.left {
+	text-align: right;
+}
+</style>
 <p>Bienvenidos!</p>
 <p>Este es un instalador, para un sistema de mesas, </p>
 <p>sigue paso a paso las instrucciones que encontraras, </p>
 <p>mas adelante.</p>
-<left>
+
 <form method="post" action="?Paso=1">
   <input type="submit" value="Iniciar">
 </form>
-</left>
+
