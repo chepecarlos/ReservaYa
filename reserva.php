d136d91fc018a14e89428aaf8f1f8a98e45999e3
Evorock <evorock@alswblog.org>
2012-03-04 17:12:51 -0600

    nombre en los campos

--- reserva.php
+++ reserva.php
@@ -1,19 +1,19 @@
 <p>Reservas Online</p>
 <form name="form1" method="post" action="">
   <p>Nombre : 
-    <input type="text" name="textfield" id="textfield">
+    <input type="text" name="Nombre" id="Nombre">
   </p>
   <p>Apellido : 
-    <input type="text" name="textfield2" id="textfield2">
+    <input type="text" name="Apellido" id="Apellido">
   </p>
   <p>Email (opcional) :
-    <input type="text" name="4" id="4">
+    <input type="text" name="Email" id="Email">
   </p>
   <p>1+1 =  
-    <input type="text" name="textfield3" id="textfield3">
+    <input type="text" name="Captcha" id="Captcha">
   </p>
   <p>Fecha de Reserva : 
-    <input type="text" name="textfield4" id="textfield4">
+    <input type="text" name="Reserva" id="Reserva">
   </p>
   <p>
     <input type="submit" name="button" id="button" value="Enviar">
