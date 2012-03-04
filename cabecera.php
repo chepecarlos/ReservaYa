a3c65887cd10bd101886af9c75902880d6f2e0ef
Evorock <evorock@alswblog.org>
2012-03-04 17:00:55 -0600

    modificacion de header

--- cabecera.php
+++ cabecera.php
@@ -1,4 +1,10 @@
-<?php
-//Cabecera de las paginas
-echo "Yo la cabeza"
-?>
+<table width="80%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td rowspan="2"><img src="../recursos/online-business-systems.jpg" width="266" height="115" /></td>
+    <td>Titulo del sitio</td>
+  </tr>
+  <tr>
+    <td>Slogan</td>
+  </tr>
+</table>
+
