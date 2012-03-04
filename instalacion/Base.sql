57cd141b71780eee30891bef13bec32260ce005c
Evorock <evorock@alswblog.org>
2012-03-04 12:06:30 -0600

    Comentarios

--- instalacion/Base.sql
+++ instalacion/Base.sql
@@ -1,16 +1,36 @@
-drop table if exists cliente;# MySQL returned an empty result set (i.e. zero rows).
+-- Base de datos para el proyecto de ADS II
+-- Tabla Cliente
+-- * Esta tabla nos ayudara para manejar la reserva
+-- * de mesas de dicho restaurante
+-- Tabla Empleado
+-- * Aqui se guardaran la informacion de cada empleado
+-- * y administrador que trabaje en el restaurante
+-- Tabla Mesa
+-- * Aqui asignaremos la cantidad de mesas que tendra
+-- * el restaurante por personas, ayudara para la 
+-- * disponibilidad de mesas que hay en x momento
+-- Tabla casual
+-- * Si existe usuario que no hagan reservas y llegue
+-- * en x momento al restaurante, se ocupara mas que 
+-- * todo para mineria de datos
+-- Tabla Reserva
+-- * Aqui es para ver la infomracion de la reserva
+-- * fecha que se realizo, dia, etc.
+-- Tabla recepcion
+-- * Verificara que los datos de la reserva sean correctos
+-- * y agregar datos para hacer una mineria de datos.
 
+drop table if exists cliente;
 create table cliente(
   id_cliente integer auto_increment not null,
   nombre varchar(50) not null,
   apellido varchar(50) not null,
   email varchar(100),
   primary key (id_cliente)
- );# MySQL returned an empty result set (i.e. zero rows).
+ );
 
 
-drop table if exists empleado;# MySQL returned an empty result set (i.e. zero rows).
-
+drop table if exists empleado;
 create table empleado(
   id_empleado integer auto_increment not null,
   nombre varchar(50) not null,
@@ -22,31 +42,27 @@ create table empleado(
   fecha_nacimiento time,
   dui int(10),
   primary key (id_empleado)
-);# MySQL returned an empty result set (i.e. zero rows).
+);
 
 
-drop table if exists mesa;# MySQL returned an empty result set (i.e. zero rows).
-
+drop table if exists mesa;
 create table mesa(
   num_personas_x_mesa int(11) not null,
   mesas int(11),
   primary key (num_personas_x_mesa)
-);# MySQL returned an empty result set (i.e. zero rows).
+);
 
-
-drop table if exists casual;# MySQL returned an empty result set (i.e. zero rows).
-
+drop table if exists casual;
 create table casual(
   id_casual integer auto_increment not null,
   num_personas_x_mesa int(11) not null,
   num_personas int(11) not null,
   primary key (id_casual),
   foreign key (num_personas_x_mesa) references mesa (num_personas_x_mesa)
-);# MySQL returned an empty result set (i.e. zero rows).
+);
 
 
-drop table if exists reserva;# MySQL returned an empty result set (i.e. zero rows).
-
+drop table if exists reserva;
 create table reserva(
   id_reserva integer auto_increment not null,
   id_cliente integer not null,
@@ -57,11 +73,10 @@ create table reserva(
   primary key (id_reserva),
   foreign key (num_personas_x_mesa) references mesa (num_personas_x_mesa),
   foreign key (id_cliente) references cliente (id_cliente)
- );# MySQL returned an empty result set (i.e. zero rows).
+ );
 
 
-drop table if exists recepcion;# MySQL returned an empty result set (i.e. zero rows).
-
+drop table if exists recepcion;
 create table recepcion(
   id_cliente integer not null,
   id_empleado integer not null,
@@ -71,4 +86,4 @@ create table recepcion(
   foreign key (id_empleado) references empleado (id_empleado),
   foreign key (id_reserva) references reserva (id_reserva),
   foreign key (id_casual) references casual (id_casual)
-);# MySQL returned an empty result set (i.e. zero rows).
+);
