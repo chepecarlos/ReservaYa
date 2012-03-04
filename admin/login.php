f1fd39708da5fb14b30dad4fd4bb922239a2fc0c
Evorock <evorock@alswblog.org>
2012-03-04 17:13:40 -0600

    login

---
+++ admin/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="">
+  <table width="80%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td>Login</td>
+      <td><input type="text" name="login" id="login" /></td>
+    </tr>
+    <tr>
+      <td>Password</td>
+      <td><input type="text" name="password" id="password" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><input type="submit" name="Aceptar" id="Aceptar" value="Enviar" />
+        Olvide Constraseña</td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>
