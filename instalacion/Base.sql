cb3c880173751abdad3e49fc0d15a4a5162bc2d2
Evorock <evorock@alswblog.org>
2012-03-04 10:57:57 -0600

    Base Finish

--- instalacion/Base.sql
+++ instalacion/Base.sql
@@ -1,15 +1,18 @@
-drop table if exists cliente;
+drop table if exists cliente;# MySQL returned an empty result set (i.e. zero rows).
+
 create table cliente(
-  id_cliente int unsigned auto_increment not null,
+  id_cliente integer auto_increment not null,
   nombre varchar(50) not null,
   apellido varchar(50) not null,
   email varchar(100),
   primary key (id_cliente)
- );
+ );# MySQL returned an empty result set (i.e. zero rows).
 
-drop table if exists empleado;
+
+drop table if exists empleado;# MySQL returned an empty result set (i.e. zero rows).
+
 create table empleado(
-  id_empleado int unsigned auto_increment not null,
+  id_empleado integer auto_increment not null,
   nombre varchar(50) not null,
   apellido varchar(50) not null,
   rol enum ('administrador', 'empleado') default 'empleado',
@@ -19,46 +22,53 @@ create table empleado(
   fecha_nacimiento time,
   dui int(10),
   primary key (id_empleado)
-);
+);# MySQL returned an empty result set (i.e. zero rows).
 
-drop table if exists mesa;
+
+drop table if exists mesa;# MySQL returned an empty result set (i.e. zero rows).
+
 create table mesa(
   num_personas_x_mesa int(11) not null,
   mesas int(11),
   primary key (num_personas_x_mesa)
-);
+);# MySQL returned an empty result set (i.e. zero rows).
 
-drop table if exists casual;
+
+drop table if exists casual;# MySQL returned an empty result set (i.e. zero rows).
+
 create table casual(
-  id_casual int unsigned auto_increment not null,
-  num_persona_x_mesa int(11) not null,
+  id_casual integer auto_increment not null,
+  num_personas_x_mesa int(11) not null,
   num_personas int(11) not null,
   primary key (id_casual),
-  foreign key (num_persona_x_mesa) references mesa (num_persona_x_mesa)
-);
+  foreign key (num_personas_x_mesa) references mesa (num_personas_x_mesa)
+);# MySQL returned an empty result set (i.e. zero rows).
 
-drop table if exists reserva;
+
+drop table if exists reserva;# MySQL returned an empty result set (i.e. zero rows).
+
 create table reserva(
-  id_reserva int unsigned auto_increment not null,
-  id_cliente int unsigned auto_increment not null,
-  num_persona_x_mesa int(11) not null,
+  id_reserva integer auto_increment not null,
+  id_cliente integer not null,
+  num_personas_x_mesa int(11) not null,
   fecha_reserva time,
   dia_reserva time, 
-  estado enum ('Pendiente','Finalizado','Progreso') not null default 'Pendiente',
+  estado enum ('Pendiente','Finalizado','Progreso') default 'Pendiente',
   primary key (id_reserva),
-  foreign key (num_persona_x_mesa) references mesa (num_persona_x_mesa),
+  foreign key (num_personas_x_mesa) references mesa (num_personas_x_mesa),
   foreign key (id_cliente) references cliente (id_cliente)
- );
+ );# MySQL returned an empty result set (i.e. zero rows).
 
 
-drop table if exists recepcion;
+drop table if exists recepcion;# MySQL returned an empty result set (i.e. zero rows).
+
 create table recepcion(
-  id_cliente int unsigned auto_increment not null,
-  id_empleado int unsigned auto_increment not null,
-  id_reserva int unsigned auto_increment not null,
-  id_casual int unsigned auto_increment not null,
+  id_cliente integer not null,
+  id_empleado integer not null,
+  id_reserva integer not null,
+  id_casual integer not null,
   foreign key (id_cliente) references cliente (id_cliente),
   foreign key (id_empleado) references empleado (id_empleado),
   foreign key (id_reserva) references reserva (id_reserva),
   foreign key (id_casual) references casual (id_casual)
-);
+);# MySQL returned an empty result set (i.e. zero rows).
