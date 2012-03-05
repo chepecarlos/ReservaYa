e324a5e8bbf73ae06184cc2b03779111e4037496
Evorock <evorock@alswblog.org>
2012-03-04 18:16:03 -0600

    Creacion de perfil y usuario

---
+++ admin/perfil.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+<p> Perfil</p>
+<form id="form1" name="form1" method="post" action="">
+  <table width="25%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td>Nombre:</td>
+      <td><input type="text" name="Nombre" id="Nombre" /></td>
+    </tr>
+    <tr>
+      <td>Apellido :</td>
+      <td><input type="text" name="Apellido" id="Apellido" /></td>
+    </tr>
+    <tr>
+      <td>Password :</td>
+      <td><input type="password" name="Password" id="Password" /></td>
+    </tr>
+    <tr>
+      <td>Email :</td>
+      <td><input type="text" name="Email" id="Email" /></td>
+    </tr>
+    <tr>
+      <td>Telefono:</td>
+      <td><input type="text" name="Telefono" id="Telefono" /></td>
+    </tr>
+    <tr>
+      <td>Fecha - Nacimiento :</td>
+      <td><input type="text" name="F_Nacimiento" id="F_Nacimiento" /></td>
+    </tr>
+    <tr>
+      <td>Dui :</td>
+      <td><input type="text" name="Dui" id="Dui" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><input type="button" name="button" id="button" value="Actualizar" /></td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>
---
+++ admin/usuarios.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+<p>Administracion de Perfiles</p>
+<form id="form1" name="form1" method="post" action="">
+  <table width="60%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td width="70%">Ususario</td>
+      <td width="70%">Nombre</td>
+      <td width="30%" colspan="2">&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>Editar </td>
+      <td>Eliminar</td>
+    </tr>
+  </table>
+</form>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
