714233f237828351ed80a964fcc52796063dcb8d
chepecarlos <chepecarlos@alswblog.org>
2012-03-04 18:09:48 -0600

    Modificanco la coneccion

--- coneccion.php
+++ coneccion.php
@@ -1,6 +1,8 @@
 <?php
 //Conexion con mysql
 require("configuracion.php");
-$conexion=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Problemas con la conexcion");
-mysql_select_db(DB_NAME,$conexion) or die("Problemas en la selección de la base de datos");
+echo DB_HOST;
+echo DB_PASSWORD;
+$conexion=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Problemas con la conexcion".mysql_error());
+mysql_select_db(DB_NAME,$conexion) or die("Problemas en la selección de la base de datos".mysql_error());
 ?>
