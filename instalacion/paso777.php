7f4312bba9e0deb80e8b5e61a97b5798da693e34
Chepecarlos <chepecarlos@alswblog.org>
2012-04-16 00:29:14 -0600

    insertando los primero datos a la base de datos desde los atributos de secion

--- instalacion/paso777.php
+++ instalacion/paso777.php
@@ -20,15 +20,18 @@ if(isset ($_REQUEST['Nombre_1'])){
 	}
 ?>
 
-consulta_0 = "insert into config values('Empresa_nombre','$_SESSION[Nombre]')"
-consulta_1 = "insert into config values('Empresa_direccion','$_SESSION[Direccion]')"
-consulta_2 = "insert into config values('Empresa_email','$_SESSION[Email]')"
-consulta_3 = "insert into config values('Empresa_website','$_SESSION[Website]')"
-consulta_4 = "insert into config values('Empresa_Telefono','$_SESSION[Telefono]')"
-consulta_5 = "insert into config values('Empresa_slogan','$_SESSION[slogan]')"
-consulta_6 = "insert into config values('Empresa_Mesas','$_SESSION[Mesas]')"
-
-consulta_7 = "insert into empleado (nombre, apellido, password, email,telefono,fecha_nacimiento,dui) values ('$_SESSION[Nombre]','$_SESSION[Apellido]','$_SESSION[Password]','$_SESSION[Email]','$_SESSION[Telefono]','$_SESSION[F_Nacimiento]','$_SESSION[Dui]')";
+<?php
+$consulta_[0] = "insert into config values('Empresa_nombre','".$_SESSION["Empresa[Nombre]"]."')";
+$consulta_[1] = "insert into config values('Empresa_direccion','".$_SESSION["Empresa[Direccion]"]."')";
+$consulta_[2] = "insert into config values('Empresa_email',".$_SESSION["Empresa[Email]"]."')";
+$consulta_[3] = "insert into config values('Empresa_website','".$_SESSION["Empresa[Website]"]."')";
+$consulta_[4] = "insert into config values('Empresa_Telefono','".$_SESSION["Empresa[Telefono]"]."')";
+$consulta_[5] = "insert into config values('Empresa_slogan','".$_SESSION["Empresa[slogan]"]."')";
+$consulta_[6] = "insert into config values('Empresa_Mesas','".$_SESSION["Empresa[Mesas]"]."')";
+$consulta_[7] = "insert into empleado (nombre, apellido, password, email,telefono,fecha_nacimiento,dui) values ('".$_SESSION["Administrador[Nombre]"]."','".$_SESSION["Administrador[Apellido]"]."','".$_SESSION["Administrador[Password]"]."','".$_SESSION["Administrador[Email]"]."','".$_SESSION["Administrador[Telefono]"]."','".$_SESSION["Administrador[F_Nacimiento]"]."','".$_SESSION["Administrador[Dui]"]."')";
+echo $consulta_[0];
+ejecutar_Sentencia($consulta_[0], $conexion);
+?>
 
 <form method="post" action="?Paso=777">
 Pagina final: 
