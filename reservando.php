b6de5012649f2a7a466be047ae35fba2c7c57d1e
Chepecarlos <chepecarlos@alswblog.org>
2012-04-22 10:41:03 -0600

    Pagina para ingresar los datos de la reserva

---
+++ reservando.php
@@ -0,0 +1,10 @@
+
+<?php
+	$VariableCaptura[0]=$_REQUEST["Nombre"];
+	$VariableCaptura[1]=$_REQUEST["Apellido"];
+	$VariableCaptura[2]=$_REQUEST["Email"];
+	$VariableCaptura[3]=$_REQUEST["Reserva"];
+	$VariableCaptura[4]=$_REQUEST["Captcha"];
+	
+	
+?>
