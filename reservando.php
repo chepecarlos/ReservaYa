a36d3a15f5169c2dac3f3020e35106edf80d9e3d
Chepecarlos <chepecarlos@alswblog.org>
2012-04-22 11:00:01 -0600

    Si no responde la pregunta se le solitica los datos de nuevo

--- reservando.php
+++ reservando.php
@@ -6,5 +6,11 @@
 	$VariableCaptura[3]=$_REQUEST["Reserva"];
 	$VariableCaptura[4]=$_REQUEST["Captcha"];
 	
-	
+	if( $VariableCaptura[4] == '2'){
+		echo "listo";
+		}
+	else{
+		echo "¿Erres real mente un humano? <br> Error el Captcha <br>";
+		require("reserva.php"); 
+		}	
 ?>
