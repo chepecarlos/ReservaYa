061cbc7fd968a1842c1130d4a683568377049846
Chepecarlos <chepecarlos@alswblog.org>
2012-04-22 11:12:37 -0600

    Verificando que ingrese un nombre y apellido

--- reservando.php
+++ reservando.php
@@ -10,7 +10,13 @@
 	$consulta_[0] = "insert into (nombre,apellido,email) cliente values('".$_SESSION["Nombre"]."','".$_SESSION["[Apellido]"]."','".$_SESSION["[Email]"]."')";
 
 	if( $VariableCaptura[4] == '2'){
-		echo "listo";
+		if($VariableCaptura[0] <> '' and $VariableCaptura[1] <> ''){
+			echo "A insertar base de datos";
+			}
+		else{
+			echo "No se ingreso en nombre y/o Apellido";
+			require("reserva.php"); 
+			}
 		}
 	else{
 		echo "<br>¿Erres real mente un humano?</br> <br> Error el Captcha </br>";
