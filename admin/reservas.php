b01e1396bc80d085d53feeb2918c844f0b5392a7
Evorock <evorock@alswblog.org>
2012-04-25 00:54:12 -0600

    tabla>

--- admin/reservas.php
+++ admin/reservas.php
@@ -1,1 +1,29 @@
 echo "Que linda son las resevas no :) ";
+
+nombre='',apellido='',password='',email='',telefono='',fecha_nacimiento=,
+dui=
+
+<table>
+<tr>
+<td>Nombre</td>
+<td>Apellido</td>
+<td>Password</td>
+<td>email</td>
+<td>telefono</td>
+<td>fecha de nacimiento</td>
+<td>dui</td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+</table>
